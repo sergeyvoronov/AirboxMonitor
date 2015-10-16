29a811de8816acc8577f3bb1fd3eb5045dd9b972
update rails --no-ri --no-rdoc <sergey.voronov@gmail.com>
2015-10-16 19:28:57 +0300

    first commit

---
+++ AirBoxMonitor/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace AirBoxMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timerSystem = new System.Windows.Forms.Timer(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(90, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timerSystem
+            // 
+            this.timerSystem.Enabled = true;
+            this.timerSystem.Interval = 1000;
+            this.timerSystem.Tick += new System.EventHandler(this.timerSystem_Tick);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(234, 192);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timerSystem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ AirBoxMonitor/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AirBoxMonitor
+{
+    public partial class Form1 : Form
+    {
+
+        TcpClient client;
+        private bool connected = true;
+        private DateTime dt;
+        private string data;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            connectToServer("10.0.0.120", 7001);
+        }
+
+        private void connectToServer(string serverHost, int port)
+        {
+       
+            try
+            {
+                client = new TcpClient();
+                client.Connect(new IPEndPoint(IPAddress.Parse(serverHost), port));
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            finally
+            {
+                client.Close();
+            }
+        }
+
+
+        private void ReadData()
+        {
+            StreamReader sr = new StreamReader(client.GetStream());
+            while (connected)
+            {
+                data = sr.ReadLine();
+                dt = DateTime.Now;
+            }
+        }
+
+        private void timerSystem_Tick(object sender, EventArgs e)
+        {
+            TimeSpan tm = DateTime.Now - dt;
+            if (tm.Seconds > 100) richTextBox1.Text += tm.ToString();
+            else
+            {
+                richTextBox1.Text += @"Ok";
+            }
+
+            richTextBox1.Text += data;
+        }
+
+    
+    }
+}
